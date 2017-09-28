83e0d3ede1ea5056194d4d9fa5767c2b16d476f3
zhanchi5 <zhanchi5@protonmail.com>
2017-09-28 09:42:05 +0300

    added tests

--- sources/print.cpp
+++ sources/print.cpp
@@ -1,6 +1,9 @@
-#include <string>
-#include <fstream>
-#include <iostream>
+#include <print.hpp>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out) {
+  out << text;
+}
+
+void print(const std::string& text, std::ofstream& out) {
+  out << text;
+}
